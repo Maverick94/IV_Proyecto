638dece1b1655aab3d2cf20085f6df182a37b932
Andrés J. Gallardo Molina <ajgm94@gmail.com>
2017-11-02 19:41:33 +0100

    Prueba de DB 2

--- botActividadesEtsiit/bot_actividad.py
+++ botActividadesEtsiit/bot_actividad.py
@@ -3,7 +3,6 @@
 
 import telebot
 import os   #Biblioteca para obtener las variables de entorno
-import urlparse
 import psycopg2
 
 
@@ -11,29 +10,23 @@ import psycopg2
 token = os.environ["TOKEN"]#Accedemos a las variables de entorno (configuradas en travis y heroku)
 bot = telebot.TeleBot(token)
 
-
-
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+	bot.reply_to(message, "Howdy, how are you doing?")
 
 @bot.message_handler(commands=['actividades'])
-def send_activity(m):
+def send_activity(message):
     """Función que envia las actividades de la semana al usuario. """
-    cid = m.chat.id # Obtenemos la id del usuario.
+    cid = message.chat.id # Obtenemos la id del usuario.
     db = os.environ["NAME_BD"]
     host_db = os.environ["HOST_BD"]
     usuario = os.environ["USER_BD"]
     pw = os.environ["PW_BD"]
-
-
     con = psycopg2.connect(database=db, user=usuario, password=pw, host=host_db)
-
     cursor = con.cursor()
     cursor.execute("SELECT * FROM Actividades")
     resp = ""
     #filas = len(cursor.fetchall())
-
     titulo = cursor.fetchone()[5]
     cursor.execute("SELECT * FROM Actividades")
     descripcion=cursor.fetchone()[6]
@@ -44,7 +37,38 @@ def send_activity(m):
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
-    bot.reply_to(message, message.text)
-
+	bot.reply_to(message, message.text)
 
 bot.polling()
+
+# @bot.message_handler(commands=['start', 'help'])
+# def send_welcome(message):
+#     bot.reply_to(message, "Howdy, how are you doing?")
+#
+# @bot.message_handler(commands=['actividades'])
+# def send_activity(m):
+#     """Función que envia las actividades de la semana al usuario. """
+#     cid = m.chat.id # Obtenemos la id del usuario.
+#     db = os.environ["NAME_BD"]
+#     host_db = os.environ["HOST_BD"]
+#     usuario = os.environ["USER_BD"]
+#     pw = os.environ["PW_BD"]
+#     con = psycopg2.connect(database=db, user=usuario, password=pw, host=host_db)
+#     cursor = con.cursor()
+#     cursor.execute("SELECT * FROM Actividades")
+#     resp = ""
+#     #filas = len(cursor.fetchall())
+#     titulo = cursor.fetchone()[5]
+#     cursor.execute("SELECT * FROM Actividades")
+#     descripcion=cursor.fetchone()[6]
+#     con.close()
+#     vuelta="Título: "+titulo+" Descripción: "+descripcion
+#
+#     bot.send_message(cid, vuelta)
+#
+# @bot.message_handler(func=lambda m: True)
+# def echo_all(message):
+#     bot.reply_to(message, message.text)
+#
+#
+# bot.polling()
