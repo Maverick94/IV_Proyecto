56f637ece047365152f2aba12971b0acd98d34b4
Andrés J. Gallardo Molina <ajgm94@gmail.com>
2017-11-21 18:13:51 +0100

    Probando cosas con la API

--- API_web.py
+++ API_web.py
@@ -1,6 +1,6 @@
 import hug
-from botActividadesEtsiit.funcionesBD import Actividad
-import json
+# from botActividadesEtsiit.funcionesBD import Actividad
+# import json
 
 
 act=Actividad()
@@ -10,15 +10,15 @@ def status():
     """Devuelve estado"""
     return { "status": "OK" }
 
-@hug.get('/actividadprueba')
-def actividades_bd():
-    consulta=act.consultarActividad(1)
-    return { "Actividad_Ejemplo": consulta }
-
-@hug.get('/actividad/{id}')
-def one(id):
-    consulta=act.consultarActividad(int(id))
-    if id == 2 or id == 1:
-        return { "Actividad": consulta }
-    else:
-        return { "Actividad": "No existe esa actividad"}
+# @hug.get('/actividadprueba')
+# def actividades_bd():
+#     consulta=act.consultarActividad(1)
+#     return { "Actividad_Ejemplo": consulta }
+#
+# @hug.get('/actividad/{id}')
+# def one(id):
+#     consulta=act.consultarActividad(int(id))
+#     if id == 2 or id == 1:
+#         return { "Actividad": consulta }
+#     else:
+#         return { "Actividad": "No existe esa actividad"}
