71d48bec9e6b27a093dd5c3006798bd4040f0a8b
David Sanchez <anixo.a.tope@gmail.com>
2017-11-23 17:26:21 +0100

    Añadido test extras

---
+++ API_web_test.py
@@ -0,0 +1,25 @@
+import hug
+import API_web
+
+def test_raiz_OK():
+    datos = hug.test.get(hugweb, '/')
+    assert datos.status == "200 OK"
+    assert datos.data['status']=="OK"
+
+def test_status_OK():
+    datos = hug.test.get(hugweb, '/status')
+    assert datos.status == "200 OK"
+    assert datos.data['status']=="OK"
+
+# def test_actividades_bd_OK():
+#     datos = hug.test.get(API_web, '/actividadprueba')
+#     assert datos.status == "200 OK"
+#     assert datos.data['Actividad_Ejemplo'] == "[(1, datetime.date(2017, 12, 5), datetime.date(2017, 12, 5), datetime.time(12, 0), datetime.time(13, 0), 'Conferencia 1', 'Esta es una conferencia de prueba que estamos haciendo para estrenar la BD')]"
+#
+# def test_one_OK():
+#     datos = hug.test.get(API_web, '/actividad/1')
+#     assert datos.status == "200 OK"
+#     assert datos.data['Actividad'] == "[(1, datetime.date(2017, 12, 5), datetime.date(2017, 12, 5), datetime.time(12, 0), datetime.time(13, 0), 'Conferencia 1', 'Esta es una conferencia de prueba que estamos haciendo para estrenar la BD')]"
+#     datos = hug.test.get(API_web, '/actividad/2')
+#     assert datos.status == "200 OK"
+#     assert datos.data['Actividad'] == "[(1, datetime.date(2017, 12, 5), datetime.date(2017, 12, 5), datetime.time(12, 0), datetime.time(13, 0), 'Conferencia 1', 'Esta es una conferencia de prueba que estamos haciendo para estrenar la BD')]"
--- Makefile
+++ Makefile
@@ -1,2 +1,3 @@
 test:
+	pytest API_web_test.py
 	cd ./botActividadesEtsiit/ && python testFunciones.py
