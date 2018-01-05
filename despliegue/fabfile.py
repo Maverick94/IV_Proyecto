a498446870dc49bd8d17576ca463b207d204f791
Andres <ajgm94@gmail.com>
2018-01-05 20:53:09 +0100

    actualizacion

--- despliegue/fabfile.py
+++ despliegue/fabfile.py
@@ -7,12 +7,14 @@ def InstalarApp():
     """ Función para descargar el bot del repositorio. """
 	# Descargamos el repositorio
     run('git clone https://github.com/Maverick94/IV_Proyecto.git')
-    run('sudo cp IV_Proyecto/apiweb.conf /etc/supervisor/conf.d/')
+
 	# Instalamos pip3
     # run('sudo apt-get install -y python3-pip')
 
 	# Accedemos al repositorio e instalamos las dependencias
+    run('sudo apt-get install supervisor')
     run('cd IV_Proyecto/ && sudo pip3 install -r requirements.txt')
+    run('sudo cp IV_Proyecto/apiweb.conf /etc/supervisor/conf.d/')
 
 def BorrarApp():
     """ Función para borrar el repositorio. """
@@ -23,10 +25,10 @@ def IniciarApp():
     """ Función para iniciar la web. """
 	# Importamos las variables globales
 	# with shell_env(TOKENBOT="454323731:AAHV_dXizf08VAkEzfMUgOKN9VaM5KCFExI"
-    run('cd ~/IV_Proyecto/ && sudo supervisorctl reread && sudo supervisorctl reload',pty=False)
+    run('cd ~/IV_Proyecto/ && sudo supervisorctl reread && sudo supervisorctl start apiweb',pty=False)
 
 def PararApp():
     """ Función para iniciar la web. """
 	# Importamos las variables globales
 	# with shell_env(TOKENBOT="454323731:AAHV_dXizf08VAkEzfMUgOKN9VaM5KCFExI"
-    run('cd ~/IV_Proyecto/ && sudo supervisorctl reread && sudo supervisorctl reload',pty=False)
+    run('sudo supervisorctl stop apiweb',pty=False)
